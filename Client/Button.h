afb884e92c80e3c24f0efa6330187606e38b67b1
Marcus Skytt <mask16@student.aau.dk>
2017-10-13 15:44:03 +0200

    Split Button into interface (Button.h) and implemenation (Button.cpp)

--- Client/Button.h
+++ Client/Button.h
@@ -5,5 +5,23 @@
 #ifndef PCSS2017_BUTTON_H
 #define PCSS2017_BUTTON_H
 
+#include <iostream>
+#include <SFML/Graphics.hpp>
+using namespace std;
+using namespace sf;
+
+class Button {
+public:
+    Texture textures[3];
+    Sprite button;
+    float buttonx;
+    float buttony;
+    string name;
+    Button();
+    void create(float x, float y, string name);
+    Sprite getSprite();
+    void setTex(int n);
+
+};
 
 #endif //PCSS2017_BUTTON_H
